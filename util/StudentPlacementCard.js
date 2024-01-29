4fb1939716017acb03a78018f8413fd35c420b5c
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-01-29 11:23:48 +0530

    Placement card update

--- util/StudentPlacementCard.js
+++ util/StudentPlacementCard.js
@@ -2,25 +2,65 @@
 import "material-icons/iconfont/material-icons.css";
 import Link from "next/link";
 
-export default function StudentPlacementCard({ placementData, cardType }) {
+export default function StudentPlacementCard({ placementData, cardType, studentId }) {
 
     const openEditPlacement = (placementID) => {
         window.open(`/dashboard/student/editPlacement/${placementID}`, "_parent");
     }
+    const openEditPlacementAdmin = (placementID) => {
+        window.open(`/dashboard/admin/student/${studentId}/editPlacement/${placementID}`, "_parent");
+    }
 
     return cardType === "1" ? (
+        // <div className="border rounded-xl backdrop-blur-xl bg-red-50 bg-opacity-30">
+        //     <div>
+        //         <div className="px-4 py-1 my-2">
+        //             <p className="font-semibold text-lg text-center bg-green-100 text-[#501515] rounded-xl w-fit px-2 m-auto">{placementData.ctc + " LPA"}</p>
+        //             <button onClick={()=>openEditPlacementAdmin(placementData.placementID)}><span className="material-icons pt-2">edit_square</span></button>
+        //         </div>
+        //         <hr className="border-gray-900 w-full" />
+        //         <div className="px-4 py-1 text-center">
+        //             <p className="font-extralight text-md text-black">
+        //                 <Link href={`/dashboard/admin/company/${placementData.companyId}`}>
+        //                     {placementData.companyName}
+        //                 </Link>
+        //             </p>
+        //             <p className="text-xs text-gray-500">{placementData["jobRole"]}</p>
+        //         </div>
+
+        //         <hr className="w-full" />
+
+        //         <div className="px-4 py-4 text-center flex flex-wrap items-center justify-center space-x-1">
+        //             {placementData["isIntern"] === "1" ? (
+        //                 <div className="bg-yellow-100 rounded-xl py-1 px-2 w-fit text-[#544a15]">Intern</div>
+        //             ) : null}
+        //             {placementData["isPPO"] === "1" ? (
+        //                 <div className="bg-green-100 rounded-xl py-1 px-2 w-fit text-[#21430e]">PPO</div>
+        //             ) : null}
+        //             {placementData["isOnCampus"] === '1' ? (
+        //                 <div className="bg-purple-100 rounded-xl py-1 px-2 w-fit text-[#1d0e3a]">On Campus</div>
+        //             ) : (
+        //                 <div className="bg-red-100 rounded-xl py-1 px-2 w-fit text-[#320f0f]">Off Campus</div>
+        //             )}
+        //             {placementData["isGirlsDrive"] === '1' ? (
+        //                 <div className="bg-pink-100 rounded-xl py-1 px-2 w-fit text-[#461348]">Girls Drive</div>
+        //             ) : (
+        //                 null
+        //             )}
+        //         </div>
+        //     </div>
+
+        // </div>
         <div className="border rounded-xl backdrop-blur-xl bg-red-50 bg-opacity-30">
             <div>
-                <div className="px-4 py-1 my-2">
+                <div className="px-4 py-1 my-2 flex m-auto align-middle">
                     <p className="font-semibold text-lg text-center bg-green-100 text-[#501515] rounded-xl w-fit px-2 m-auto">{placementData.ctc + " LPA"}</p>
+                    {/* <Link href={"/dashboard/student/editPlacement/"+placementID}><span className="material-icons">edit_square</span></Link> */}
+                    <button onClick={()=>openEditPlacementAdmin(placementData.placementID)}><span className="material-icons pt-2">edit_square</span></button>
                 </div>
                 <hr className="border-gray-900 w-full" />
                 <div className="px-4 py-1 text-center">
-                    <p className="font-extralight text-md text-black">
-                        <Link href={`/dashboard/admin/company/${placementData.companyId}`}>
-                            {placementData.companyName}
-                        </Link>
-                    </p>
+                    <p className="font-extralight text-md text-black">{placementData.companyName}</p>
                     <p className="text-xs text-gray-500">{placementData["jobRole"]}</p>
                 </div>
 

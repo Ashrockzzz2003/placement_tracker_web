7306e23fe72d515ec44a41c1af801d7b99ddcba4
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2025-01-18 23:14:45 +0530

    Add util/ to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./util/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
